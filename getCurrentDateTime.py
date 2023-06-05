fcba56f388f35623ddcbe7e3e5853db3bf3a5cc8
Pedro Jambers Trevisani <101676180+pedrojambers@users.noreply.github.com>
2023-06-05 19:47:37 -0300

    Delete getCurrentDateTime.py

--- getCurrentDateTime.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def lambda_handler(event, context):
-    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    response = {
-        'statusCode': 200,
-        'body': current_datetime
-    }
-    return response
